3066544b47fc735efe4830773fde7ef0dc5be4cd
mrityunjayshukla411 <69010998+mrityunjayshukla411@users.noreply.github.com>
2021-10-18 15:47:10 +0530

    about page completed

---
+++ client/src/pages/about/About.js
@@ -0,0 +1,14 @@
+import AboutPageBody from "./about-page-body";
+import './about-page.css'
+import image from '../../images/home-page-image.jpeg';
+import stars from '../../images/stars.png';
+
+function App() {
+  return (
+    <>
+      <AboutPageBody></AboutPageBody>
+    </>
+  );
+}
+
+export default App;
