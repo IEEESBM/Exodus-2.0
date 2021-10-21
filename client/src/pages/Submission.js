b39bc558822f457d0b93b6e66c9d15a079c026a1
Praveen Varma <swamiraju.varma@learner.manipal.edu>
2021-10-21 22:52:45 +0530

    submission-page, some work left

--- client/src/pages/Submission.js
+++ client/src/pages/Submission.js
@@ -1,9 +1,32 @@
 import React from "react";
+import "./submission-styles.css";
 
 const Submission = () => {
-    return(
-        <h1>Submission</h1>
-    )
-}
+  return (
+    <div>
+      <h1 className="title">SUBMISSION FORM</h1>
 
-export default Submission
+      <form action="">
+        <div>
+          <label htmlFor="gname">Group Name: </label>
+          <input type="text" id="gname" name="gname" />
+        </div>
+        <div>
+          <label htmlFor="aid">Application ID: </label>
+          <input type="text" id="aid" name="aid" />
+        </div>
+        <div>
+          <label htmlFor="topic">Topic: </label>
+          <input type="text" id="topic" name="topic" />
+        </div>
+        <div>
+          <label htmlFor="link">Submission Link: </label>
+          <input type="url" id="link" name="link" />
+        </div>
+
+        <button type="submit">SUBMIT</button>
+      </form>
+    </div>
+  );
+};
+export default Submission;
