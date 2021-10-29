7b52dee1c76280b61c2f891d2ae623b73ed447f0
Ayush Pandey <73249644+heisenberg-ayush@users.noreply.github.com>
2021-10-30 01:20:17 +0530

    Submission Form(Animation Not Done)

--- client/src/pages/submission/Submission.js
+++ client/src/pages/submission/Submission.js
@@ -1,10 +1,11 @@
 import SubmissionPageBody from "./submission-page-body";
-import './submission-page.css'
+import './submission-page.css';
+import rocket from '../../images/rocket.svg';
 
 function App() {
   return (
     <>
-      <SubmissionPageBody></SubmissionPageBody>
+      <SubmissionPageBody imge={rocket} />
     </>
   );
 }
