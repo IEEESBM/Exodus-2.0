860d651101625c71e7a59547de28ca6d40434b69
divyashree <divyamanipal01@gmail.com>
2021-11-14 00:10:09 +0530

    Changes

--- client/src/pages/submission/Submission.js
+++ client/src/pages/submission/Submission.js
@@ -137,6 +137,7 @@ function Submission(props) {
             value={topic} 
             validations={[required]} 
             onChange={onChangeTopic}>
+              <option value=""></option>
                 <option value="Interplanetary Airline">Interplanetary Airline</option>
                 <option value="Cryopreservation">Cryopreservation</option>
                 <option value="Cure it all Pharmacy">Cure it all Pharmacy</option>
